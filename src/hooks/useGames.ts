f34d468c90e37b53d9218d222322b5a2b5e4d623
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-06-08 01:20:09 +0530

    Refactoring - remove dublicate interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,14 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
-import { FetchResponse } from '../services/api-client'; 
+import { FetchResponse } from '../services/api-client';
 import { Genre } from './useGenres';
 import apiClient from '../services/api-client';
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+import { Platform } from './usePlatforms';
 
 export interface Game {
   id: number;
