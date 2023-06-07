ec70f5b120d35c9d64b2563ca39aaead3be4d8ac
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-06-08 01:05:56 +0530

    Refactoring - fetch games with React Quesry

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,10 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
-import { GameQuery } from "../App";
-import useGames, { Platform } from "../hooks/useGames";
-import { Genre } from "../hooks/useGenres";
-import GameCard from "./GameCard";
-import GameCardContainer from "./GameCardContainer";
-import GameCardSkeleton from "./GameCardSkeleton";
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import { GameQuery } from '../App';
+import useGames, { Platform } from '../hooks/useGames';
+import { Genre } from '../hooks/useGenres';
+import GameCard from './GameCard';
+import GameCardContainer from './GameCardContainer';
+import GameCardSkeleton from './GameCardSkeleton';
 
 interface Props {
   gameQuery: GameQuery;
@@ -14,7 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -28,7 +28,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,8 @@
-import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import { useQuery } from '@tanstack/react-query';
+import { GameQuery } from '../App';
+import { FetchResponse } from '../services/api-client'; 
+import { Genre } from './useGenres';
+import apiClient from '../services/api-client';
 
 export interface Platform {
   id: number;
@@ -18,17 +20,34 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>('/games', {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
+
+// useQuery<FetchResponse<Game>, Error>({
+//   queryKey: ['games', gameQuery],
+//   queryFn: () =>
+//     apiClient
+//       .get<FetchResponse<Game>>('/games', {
+//         params: {
+//           genres: gameQuery.genre?.id,
+//           parent_platforms: gameQuery.platform?.id,
+//           ordering: gameQuery.sortOrder,
+//           search: gameQuery.searchText,
+//         },
+//       })
+//       .then((res) => res.data),
+// });
 
 export default useGames;
