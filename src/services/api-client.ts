619f63b48efca5d6a1b286a4f99cee6cb7308a45
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-06-07 23:31:02 +0530

    Refactor genres & platforms hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: 'https://api.rawg.io/api',
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: 'f3abfdb4682f4be2acb684d96404480e',
   },
 });
