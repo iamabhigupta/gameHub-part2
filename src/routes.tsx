32ce531803074140da265f5df9ac1d6523603aab
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-02 18:22:07 +0530

    Handle error

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
 import GameDetailsPage from './pages/GameDetailsPage';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
       {
