4ce96b7a3bbb63dd5acb8376fcec23824ceebf21
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-02 21:03:43 +0530

    Fetch game details

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,7 +1,22 @@
+import { Heading, Spinner, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/usegame';
 
 const GameDetailsPage = () => {
-  return <div>GameDetailsPage</div>;
+  const { id } = useParams();
+  const { data: game, isLoading, error } = useGame(id!);
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailsPage;
