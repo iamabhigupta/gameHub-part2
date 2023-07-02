1d1bcf9055dc72e5f9d09f58891a097a86319e51
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-02 21:11:36 +0530

    Refactor - move entity interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App';
 import theme from './theme';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
