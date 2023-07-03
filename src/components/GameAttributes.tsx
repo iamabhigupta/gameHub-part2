8a90ec7fa609088eb5d42fc7c07b432faac3acc8
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-03 13:12:54 +0530

    Build games attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,35 @@
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import { Game } from '../entities/Game';
+import CriticScore from './CriticScore';
+import DefinationItem from './DefinationItem';
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <SimpleGrid columns={2} as="dl">
+      <DefinationItem term="Platform">
+        {game.parent_platforms?.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefinationItem>
+      <DefinationItem term="Metascore">
+        <CriticScore score={game.metacritic} />
+      </DefinationItem>
+      <DefinationItem term="Genres">
+        {game.genres?.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </DefinationItem>
+      <DefinationItem term="Publishers">
+        {game.publishers?.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </DefinationItem>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,7 +1,7 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
+import GameAttributes from '../components/GameAttributes';
 import useGame from '../hooks/usegame';
 
 const GameDetailsPage = () => {
@@ -16,6 +16,7 @@ const GameDetailsPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
