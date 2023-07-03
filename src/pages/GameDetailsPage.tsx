775dedf093d9f0a975853f8acb0fbb1f69e7f898
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-03 13:55:37 +0530

    Build game trailer

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
+import GameTrailer from '../components/GameTrailer';
 import useGame from '../hooks/usegame';
 
 const GameDetailsPage = () => {
@@ -17,6 +18,7 @@ const GameDetailsPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
