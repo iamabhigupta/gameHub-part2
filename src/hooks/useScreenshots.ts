2e7840d14a513d7050347410f876e9f7209e5456
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-03 19:29:01 +0530

    Fix navbar

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -2,11 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { Screenshots } from '../entities/Screenshot';
 import APIClient from '../services/api-client';
 
-interface Props {
-  gameId: number;
-}
-
-const useScreenshots = ({ gameId }: Props) => {
+const useScreenshots = (gameId: number) => {
   const apiClient = new APIClient<Screenshots>(`/games/${gameId}/screenshots`);
 
   return useQuery({
--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,5 +1,6 @@
-import { Heading, Spinner } from '@chakra-ui/react';
+import { GridItem, Heading, SimpleGrid, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
 import GameScreenshots from '../components/GameScreenshots';
@@ -16,11 +17,17 @@ const GameDetailsPage = () => {
 
   return (
     <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <GridItem>
+          <Heading>{game.name}</Heading>
+          <ExpandableText>{game.description_raw}</ExpandableText>
+          <GameAttributes game={game} />
+        </GridItem>
+        <GridItem>
+          <GameTrailer gameId={game.id} />
+          <GameScreenshots gameId={game.id} />
+        </GridItem>
+      </SimpleGrid>
     </>
   );
 };
